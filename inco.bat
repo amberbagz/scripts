37ef6120db830f466cade942cf56a29643bc75b6
amberbagz <amsky007@gmail.com>
2025-03-30 14:48:50 +0530

    Added firefox

--- inco.bat
+++ inco.bat
@@ -1,1 +1,5 @@
-start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave" "https://reddit.com" "https://instagram.com" "https://web.whatsapp.com/" "https://stackoverflow.com/questions/43871637" "https://google.com" --incognito
+start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave" "https://reddit.com" "https://instagram.com" "https://stackoverflow.com/questions/43871637" "https://google.com" --incognito
+
+start "" "firefox" --private-window "https://reddit.com/"
+start "" "C:\Program Files\Mozilla Firefox\firefox" --private-window "https://instagram.com"
+start "" "firefox" --private-window "https://web.whatsapp.com/"
